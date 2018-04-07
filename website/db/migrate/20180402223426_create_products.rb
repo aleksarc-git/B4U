9ec052047d004f5a259f306fc3596ab031877971
aleksarc.git <aleksarc.git@gmail.com>
2018-04-07 17:26:42 -0400

    update poroducts image and logout link

--- website/db/migrate/20180402223426_create_products.rb
+++ website/db/migrate/20180402223426_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :image
       t.string :name
       t.string :description
       t.float :price
